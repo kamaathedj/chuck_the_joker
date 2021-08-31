0356c2c47e655132d2648a2059167a46b3373a1e
kamaathedj <kamaudavikiruku@gmail.com>
2021-08-31 10:47:00 +0300

    :sparkless: added new features to the app

---
+++ src/utilities/category.js
@@ -0,0 +1,4 @@
+const categories = ["food","dev",'travel','sport','science','religion','political','music',
+'movie','money','history','fashion','career','celebrity','animal','explicit'];
+
+export default categories
