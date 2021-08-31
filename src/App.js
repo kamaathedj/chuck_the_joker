ee6d202e6de9b26cbca54fed33b51a3f7336b47a
kamaathedj <kamaudavikiruku@gmail.com>
2021-08-31 10:46:21 +0300

    :art: added new features to the app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import { useStoreState,useStoreActions } from 'easy-peasy';
-function App() {
-  const {jokes,loadingState} = useStoreState((state) => state.jokes);
-  const getjokes = useStoreActions((state) => state.jokes.getJokes);
+import styled from 'styled-components';
+import Joke from './components/joke'
+import categories from './utilities/category';
+import state from './utilities/utils';
 
-  console.log(loadingState)
 
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0.5em 0.2em;
+  padding: 0.25em 1em;
+  
+  
+  &:hover {
+    background:palevioletred;
+    color:white 
+  }
+  `;
+  
+  const Wrapper = styled.div`
+  display: inline-block;
+  align-items: center;
+  
+  
+  `;
+  const ScentedBox = styled.div`
+  color: palevioletred;
+  border: 2px solid palevioletred;
+  border-radius: 30px;
+  padding:10px;
+  
+  `;
+
+
+
+const App = ()=> {
+  const {getJokes} = useStoreActions((state) => state.jokes);
+  const {loadingState} = useStoreState((state) => state.jokes);
+
+  function handleClick(e){
+    e.preventDefault()
+    getJokes(e.currentTarget.innerHTML)
+  }
   useEffect(() => {
-    getjokes('food')
+    getJokes('dev')
     // eslint-disable-next-line
   }, []);
   return (
-   
-    <div>
-      <p>
-        {jokes.value}
-      </p>
-    </div>
+    <Wrapper>
+      {categories.map(it => (
+        
+        <Button key = {it}onClick={handleClick}>{it}</Button>  
+      ))}
+      <ScentedBox>
+        {loadingState === state.loading ? <h3>loading ....</h3>:<Joke/>}
+      </ScentedBox>
+    </Wrapper>
   );
 }
 
